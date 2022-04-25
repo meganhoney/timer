7b3ca96eca3af5291392cce9a13e84a7c3efaa9f
Megan Honey <mjthoney@gmail.com>
2022-04-25 20:20:22 +0000

    Add conditionals for negative or non number inputs

--- timer1.js
+++ timer1.js
@@ -1,12 +1,16 @@
 const times = process.argv.slice(2);
 
 if (times.length === 0) {
-  process.stdout.write('Please input a timer\n');
+  process.stdout.write('Please input a timer.\n');
 }
 
 for (let time in times) {
-  setTimeout(() => {
-    process.stdout.write('\x07');
-    console.log('(', times[time], ')');
-  }, times[time] * 1000);
+  if (times[time] > 0 && typeof parseInt(times[time]) === 'number') {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      console.log('(', times[time], ') seconds have elapsed.');
+    }, times[time] * 1000);
+  } else {
+    process.stdout.write('Please input a valid timer.\n');
+  }
 }
