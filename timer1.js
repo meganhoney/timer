fa9f78eec4bf50d7f7566357fe9ccb1ee8bc9a28
Megan Honey <mjthoney@gmail.com>
2022-04-25 19:56:26 +0000

    Add condition if no numbers are inputted

--- timer1.js
+++ timer1.js
@@ -1,5 +1,9 @@
 const times = process.argv.slice(2);
 
+if (times.length === 0) {
+  process.stdout.write('Please input a timer\n');
+}
+
 for (let time in times) {
   setTimeout(() => {
     process.stdout.write('\x07');
